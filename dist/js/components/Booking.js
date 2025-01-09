abc786d44fffba3532de5f272d273b708fe97cb8
Paulina Partyka <paulinapartyka2@gmail.com>
2025-01-09 20:38:20 +0100

    add booking class

---
+++ dist/js/components/Booking.js
@@ -0,0 +1,43 @@
+import {select,templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+    constructor(element){
+        const thisBooking=this;
+
+        thisBooking.element=element;
+
+        thisBooking.render();
+        thisBooking.initWidget();
+    }
+
+    render(){
+        const thisBooking=this;
+
+        const generateHTML=templates.bookingWidget();
+
+        thisBooking.dom={};
+
+        thisBooking.dom.wrapper=thisBooking.element;
+
+        thisBooking.dom.wrapper.innerHTML=generateHTML;
+
+        thisBooking.dom.peopleAmount=thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+
+        thisBooking.dom.hoursAmount=thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    }
+
+    initWidget(){
+        const thisBooking=this;
+
+        thisBooking.peopleAmount= new AmountWidget(thisBooking.dom.peopleAmount);
+
+        thisBooking.hoursAmount-new AmountWidget(thisBooking.dom.hoursAmount);
+
+        thisBooking.dom.peopleAmount.addEventListener('click', function(){});
+
+        thisBooking.dom.hoursAmount.addEventListener('click', function(){});
+
+    }
+}
+export default Booking;
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,64 @@
-import {settings,select} from './settings.js';
+import {settings,select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp=this;
 
-    initMenu: function (){
+    thisApp.pages=document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks=document.querySelectorAll(select.nav.links);
+
+    const idFromHash=window.location.hash.replace('#/','');
+    //console.log(idFromHash);
+
+    let pageMatchingHash=thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if (page.id==idFromHash){
+        pageMatchingHash=page.id;
+        break
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement=this;
+        event.preventDefault();
+
+        //get page id from href attribute
+        const id=clickedElement.getAttribute('href').replace('#','');
+
+        //run thisApp.activatePage with that id
+
+        thisApp.activatePage(id);
+
+        // change URL hash
+        window.location.hash='#/'+id;
+      })
+    }
+  },
+
+  activatePage:function(pageId){
+    const thisApp=this;
+
+    //add class 'active' to matching pages, remove from non-matching
+    for (let page of thisApp.pages){
+         // page.classList.remove(classList.pages.active);
+         page.classList.toggle(classNames.pages.active, page.id==pageId)
+    }
+       //add class 'active' to matching links, remove from non-matching
+       for (let link of thisApp.navLinks){
+        // page.classList.remove(classList.pages.active);
+        link.classList.toggle(classNames.nav.active, link.getAttribute('href')=="#"+pageId)
+   }
+  },
+
+  initMenu: function (){
       // this method create for every product ,,new Product"->menu
       const thisApp=this;
 
@@ -13,9 +67,9 @@ const app = {
       for (let productData in thisApp.data.products){
         new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
-    },
+  },
 
-    initData: function (){
+  initData: function (){
       const thisApp=this;
 
       thisApp.data={};
@@ -36,9 +90,9 @@ const app = {
         });
 
         //console.log('thisApp.data', JSON.stringify(thisApp.data));
-    },
+  },
 
-    init: function(){
+  init: function(){
       const thisApp = this;
       //console.log('*** App starting ***');
       //console.log('thisApp:', thisApp);
@@ -48,9 +102,11 @@ const app = {
 
       thisApp.initData();
       thisApp.initCart();
-    },
+      thisApp.initPages();
+      thisApp.initBooking();
+  },
 
-    initCart: function(){
+  initCart: function(){
       const thisApp=this;
 
       const cartElem=document.querySelector(select.containerOf.cart);
@@ -60,7 +116,15 @@ const app = {
       thisApp.productList.addEventListener('add-to-cart', function(event){
         app.cart.add(event.detail.product);
       })
-    }
+  },
+
+  initBooking:function(){
+    const thisApp=this;
+    const bookingWidget=document.querySelector(select.containerOf.booking);
+
+    thisApp.booking=new Booking(bookingWidget);
+    //console.log(bookingWidget);
+  }
 };
 
 app.init();
